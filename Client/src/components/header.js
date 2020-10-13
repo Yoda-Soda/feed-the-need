e32a27ca0f367da8a8503b756392c3027154a5a4
d4cro <33950559+davecrowe55@users.noreply.github.com>
2020-10-14 12:27:46 +1300

    delete these
    delete these

---
+++ Client/src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+const styledHeader = (props) => {
+    return (<header className="App-header">
+    welcome 
+    </header>);
+}
+
+export default styledHeader;
