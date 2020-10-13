e1e5ce8937bc8760e5b35dc29d92c30eabc9c4cf
Yoda-Soda <janclaasen@gmail.com>
2020-10-14 00:45:02 +1300

    added middle ware to check routes for auth

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.EXPRESS_PORT || 5000;
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://yodasoda.au.auth0.com/.well-known/jwks.json",
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: process.env.AUTH0_URL,
+  algorithms: ["RS256"],
+});
+
+app.use(jwtCheck); //middleware will check all routes for auth
+
 app.get("/", (req, res) => {
   res.send(
     "Hello Kelly, Katie, Dave, Jan, Mike. Good luck with the Sausage Roll TEamwork!"
