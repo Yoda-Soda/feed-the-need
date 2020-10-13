c79d843b8e81216e15a40af190e311838426285f
MichaelBotur <62525995+MichaelBotur@users.noreply.github.com>
2020-10-13 13:12:10 +1300

    Pushing up

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WELCOME TO THE TRADE APP OF JAN, KELLY, KATE, DAVE AND MIKE AND JO <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
