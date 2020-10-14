f85219f755d23a317eeebc7a89b1254e3b49d234
Katiepietatie <k.miles09@gmail.com>
2020-10-15 10:59:24 +1300

    Update alldata.sql

--- db/sql/alldata.sql
+++ db/sql/alldata.sql
@@ -27,8 +27,3 @@ create table item (
 insert into donorswithlistings (firstname_username, lastname_username, emailaddress, linktophotoofdonation) values ('Michael', 'Botur', 'michaelbotur@developersinstitute.co.nz', 'https://teneightymagazine.com/wp-content/uploads/2019/07/ffa82ad4-937e-412c-9bfd-33cb9252e88e-instagram-donate.jpg');
 insert into item (firstname_username, lastname_username, emailaddress, linktophotoofdonation, item_id) values ('Michael', 'Botur', 'michaelbotur@developersinstitute.co.nz', 'https://teneightymagazine.com/wp-content/uploads/2019/07/ffa82ad4-937e-412c-9bfd-33cb9252e88e-instagram-donate.jpg',1);
 insert into recipientsandrequesters (firstname_username, lastname_username, emailaddress) values ('Shaun', 'Ferguson', 'shaunferguson@developersinstitute.co.nz');
-
-
- 
- 
-
