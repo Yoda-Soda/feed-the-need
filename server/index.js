f80af6c85113163e5e61c64b1c513d28a63ed3d6
MichaelBotur <62525995+MichaelBotur@users.noreply.github.com>
2020-10-15 13:29:36 +1300

    Swagger docs created with openapi.yml file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.EXPRESS_PORT || 5000;
 const jwt = require("express-jwt");
 const jwks = require("jwks-rsa");
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = yaml.safeLoad(
+  fs.readFileSync(path.join(__dirname, "/openapi.yml"), "utf8"));
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
     cache: true,
@@ -16,6 +18,7 @@ const jwtCheck = jwt({
   algorithms: ["RS256"],
 });
 
+app.use("/api", apiRouter);
 app.use(jwtCheck); //middleware will check all routes for auth
 
 app.get("/", (req, res) => {
@@ -26,3 +29,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+
+
+
+            
+          
+              
