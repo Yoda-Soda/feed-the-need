1350fc91be7a593c3ea25af28a4b10b276a17af3
Yoda-Soda <janclaasen@gmail.com>
2020-10-15 13:12:46 +1300

    refractored authCheck

--- server/index.js
+++ server/index.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.EXPRESS_PORT || 5000;
-const jwt = require("express-jwt");
-const jwks = require("jwks-rsa");
-
-const jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: "https://yodasoda.au.auth0.com/.well-known/jwks.json",
-  }),
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_URL,
-  algorithms: ["RS256"],
-});
+const jwtCheck = require("./jwt-check");
 
 app.use(jwtCheck); //middleware will check all routes for auth
 
