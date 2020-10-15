fe1af579cbc27cc9b2acac30957d3e03c2ce3de8
d4cro <33950559+davecrowe55@users.noreply.github.com>
2020-10-15 13:13:07 +1300

    Added get route, renamed router to app, working.

--- server/router/api_router.js
+++ server/router/api_router.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const router = express.Router();
+const app = express.Router();
 
-router.post("/listings", async (req, res) => {
+app.post("/listings", async (req, res) => {
     try {
       const { body } = req;
     //   const newlisting = await pool.query(
@@ -16,8 +16,24 @@ router.post("/listings", async (req, res) => {
       return res.status(400).end();
     }
   });
+
+  app.get("/listings", async (req, res) => {
+    try {
+      const { body } = req;
+    //   const newlisting = await pool.query(
+    //     "CREATE listing SET list_id = $1, donor_id = $2, date_created = $3, title = $4, description = $5 ", //Needs correct formatting still
+    //     []
+    //   );
+  
+    //   return res.json(newlisting);
+    return res.status(200).send("get listings success");
+    } catch (error) {
+      console.log(error);
+      return res.status(400).end();
+    }
+  });
   
 
-  module.exports = router;
+  module.exports = app;
 
   
