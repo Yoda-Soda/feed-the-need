20d63d6496475f110b18d94fd7b060052c816945
Katiepietatie <k.miles09@gmail.com>
2020-10-15 15:33:32 +1300

    Added Schema and dummy data for tables user_account list

--- db/sql/alldata.sql
+++ db/sql/alldata.sql
@@ -1,29 +1,19 @@
-create table donorswithlistings (
-	id serial PRIMARY KEY,
-	firstname_username VARCHAR(30) NOT NULL,
-	lastname_username VARCHAR(30) NOT NULL,
-	emailaddress VARCHAR(50) NOT NULL,
-	linktophotoofdonation VARCHAR(200) NOT NULL,
-	timestamp_when_donated TIMESTAMP DEFAULT NOW()
+CREATE TABLE user_account (
+	id SERIAL PRIMARY KEY,	
+	email TEXT NOT NULL UNIQUE
+);
+insert into user_account (email) values ( 'michaelbotur@developersinstitute.co.nz' );
+insert into user_account (email) values ( 'katiemiles@developersinstitute.co.nz' );
+
+CREATE TABLE list (
+	id SERIAL PRIMARY KEY,
+	donor_id int,
+	title text,
+	description text,
+	date_created TIMESTAMP DEFAULT NOW(),
+	CONSTRAINT fk_user_account
+		FOREIGN KEY(donor_id)
+		REFERENCES user_account(id) 	
 );
 
-create table recipientsandrequesters (
-	id serial PRIMARY KEY,
-	firstname_username VARCHAR(30) NOT NULL,
-	lastname_username VARCHAR(30) NOT NULL,
-	emailaddress VARCHAR(50) NOT NULL,
-	timestamp_when_requested TIMESTAMP DEFAULT NOW()
-);
-
-create table item (
-	id serial PRIMARY KEY,
-	firstname_username VARCHAR(30) NOT NULL,
-	lastname_username VARCHAR(30) NOT NULL,
-	emailaddress VARCHAR(50) NOT NULL,
-	linktophotoofdonation VARCHAR(200) NOT NULL,
-	item_id INTEGER
-);
-
-insert into donorswithlistings (firstname_username, lastname_username, emailaddress, linktophotoofdonation) values ('Michael', 'Botur', 'michaelbotur@developersinstitute.co.nz', 'https://teneightymagazine.com/wp-content/uploads/2019/07/ffa82ad4-937e-412c-9bfd-33cb9252e88e-instagram-donate.jpg');
-insert into item (firstname_username, lastname_username, emailaddress, linktophotoofdonation, item_id) values ('Michael', 'Botur', 'michaelbotur@developersinstitute.co.nz', 'https://teneightymagazine.com/wp-content/uploads/2019/07/ffa82ad4-937e-412c-9bfd-33cb9252e88e-instagram-donate.jpg',1);
-insert into recipientsandrequesters (firstname_username, lastname_username, emailaddress) values ('Shaun', 'Ferguson', 'shaunferguson@developersinstitute.co.nz');
+insert into list (donor_id, title, description) values ( 1, 'My Item', 'My Item Description' );
