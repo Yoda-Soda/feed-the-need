f8d17d6731d9ff862761a6c669d0341644704862
Katiepietatie <k.miles09@gmail.com>
2020-10-20 12:54:22 +1300

    Fetc request complete

--- client/src/components/views/ViewListingPage.js
+++ client/src/components/views/ViewListingPage.js
@@ -6,39 +6,45 @@ import {
     Route,
     useParams
   } from "react-router-dom";
-
+import { useAuth0 } from "@auth0/auth0-react"
 import { Container, Button, Grid, Box, Typography } from "@material-ui/core";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 
 const ViewListingPage = () => {
+    const { getAccessTokenSilently } = useAuth0();
 
     // TODO remove dummy data from default state
-    const [listingData, setListingData] = useState({title: "test-title", description: "test-description", listingId: 1 });
+    const [listingData, setListingData] = useState({});
+    // { title: "test-title", description: "test-description", listingId: 1 }
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const { listingId } = useParams();
+
     const getListingData = async () => {
         try {
+            const myToken = await getAccessTokenSilently();
             // TODO - use the environ variable instead of hardcoded url
-            // const result = await fetch(`http://localhost:5123/api/listings/${listingId},`
-            // { headers: "Content-Type": "application/json" })
+            const result = await fetch(`http://localhost:5123/api/listings/${listingId}`,
+            { headers: {"Content-Type": "application/json", 'Authorization' : `Bearer ${myToken}` }})
             //TODO - remove dummy data, replace with fetch call as above.
-            const result = JSON.stringify({title: "test-title", description: "test-description", listingId: 1 });
             const resultJson = await result.json();
             console.log(resultJson);  
-            setListingData(resultJson);      
+            setListingData(resultJson); 
+            console.log(listingData); 
+
         } catch (e) {
             console.error(e.message);
         } finally {
             
         }
     }
+
     useEffect(() => {
         getListingData();
       }, []);
-
-    const { listingId } = useParams();
+    
     
     // styles
     const h1 = {
@@ -63,10 +69,11 @@ const ViewListingPage = () => {
             
             <Typography component="div">
             <Box fontSize="h1.fontSize" style={h1} >
-                Title { listingData.title }
+                {/* Title { listingData.title } */}
             </Box>
             <Box fontSize="fontSize" m={1}>
-            Description Description Description Description Description Description Description Description Description Description Description Description{ listingData.description }
+            Description Description Description Description Description Description Description Description Description Description Description Description
+            {/* { listingData.description } */}
             </Box>  
             </Typography>            
                 <Button m={1} href="/listings" variant="contained" color="primary" startIcon={<ArrowBackIcon />}>
