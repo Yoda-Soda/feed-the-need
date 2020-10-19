7b60081d9f799b98c1428007706bd1a73cb8bd84
Yoda-Soda <janclaasen@gmail.com>
2020-10-19 17:01:40 +1300

    refactor isPositiveInt and subrouter

---
+++ server/router/subRouter/listingRouter.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express.Router();
+const pool = require("../../db");
+const { isPositiveInt } = require("../router_utilities/is_positive_integer");
+
+/****  post /api/listings - post single listing  ****/
+app.post("/listings", async (req, res) => {
+  try {
+    const { donor_id, title, description } = req.body;
+    console.log(req.body);
+    if (
+      donor_id === undefined ||
+      title === undefined ||
+      description === undefined
+    ) {
+      return res.status(400).send("Bad Request - missing parameter/s");
+    }
+    if (!isPositiveInt(donor_id)) {
+      return res.status(400).send("Bad Request - donor_id is not positiveInt");
+    }
+    const newlisting = await pool.query(
+      `INSERT INTO list (donor_id, title, description) values ( $1, $2, $3 )`,
+      [1, title, description]
+    );
+    res.status(201).json("OK - list was updated");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+});
+
+module.exports = app;
