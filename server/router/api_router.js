eb9a4688d9206280935fb07ee3cdc1f2be16bef6
mark churcher <mark@developersinstitute.ac.nz>
2020-10-21 15:36:51 +1300

    CGP-21 CGP-30 tweak user upsert logic

--- server/router/api_router.js
+++ server/router/api_router.js
@@ -7,7 +7,8 @@ const isPositiveInt = (intToBeTested) =>
 
 app.post("/listings", async (req, res) => {
   try {
-   const email = req.user[`${process.env.DOMAIN}email`];
+      //console.log(req.user);
+   const email = req.user[`http://feedtheneed.click/email`];
     const { donor_id, title, description } = req.body;
     if (
       donor_id === undefined ||
@@ -19,13 +20,26 @@ app.post("/listings", async (req, res) => {
     if (!isPositiveInt(donor_id)) {
       return res.status(400).send("Bad Request - donor_id is not positiveInt");
     }
-    const newEmail = await pool.query(
-       `INSERT INTO user_account (email) SELECT ($2) WHERE NOT EXISTS (SELECT id from user_account WHERE email = $1) returning id`,
-       [email, email]);
-    console.log(newEmail.rows);
+let userId;
+    const dbResult = await pool.query(
+        `SELECT id from user_account where email = $1`,
+        [email]);
+       
+        if(!dbResult.rows[0]) {
+            //user does not exist, add it!
+            const insertResult = await pool.query(
+                `INSERT INTO user_account (email) VALUES ($1) returning id`,
+                [email]);
+            userId = insertResult.rows[0].id;
+        } else {
+            userId = dbResult.rows[0].id;
+        }
+        
+    // case where no user exists!
+    
     const newlisting = await pool.query(
       `INSERT INTO list (donor_id, title, description) values ( $1, $2, $3 )`,
-      [newEmail.rows[0].id, title, description]
+      [userId, title, description]
     );
     res.status(201).json("OK - list was updated");
   } catch (error) {
