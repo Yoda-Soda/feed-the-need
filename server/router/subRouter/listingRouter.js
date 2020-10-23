a2e8bb6ab21e172dbc36c4c613082ad2359a6bdd
Yoda-Soda <janclaasen@gmail.com>
2020-10-23 15:08:42 +1300

    readded post /api/listings route from master(bad merge)

--- server/router/subRouter/listingRouter.js
+++ server/router/subRouter/listingRouter.js
@@ -6,8 +6,9 @@ const { isPositiveInt } = require("../router_utilities/is_positive_integer");
 /****  post /api/listings - post single listing  ****/
 app.post("/", async (req, res) => {
   try {
+    //console.log(req.user);
+    const email = req.user[`http://feedtheneed.click/email`];
     const { donor_id, title, description } = req.body;
-    console.log(req.body);
     if (
       donor_id === undefined ||
       title === undefined ||
@@ -18,9 +19,28 @@ app.post("/", async (req, res) => {
     if (!isPositiveInt(donor_id)) {
       return res.status(400).send("Bad Request - donor_id is not positiveInt");
     }
+    let userId;
+    const dbResult = await pool.query(
+      `SELECT id from user_account where email = $1`,
+      [email]
+    );
+
+    if (!dbResult.rows[0]) {
+      //user does not exist, add it!
+      const insertResult = await pool.query(
+        `INSERT INTO user_account (email) VALUES ($1) returning id`,
+        [email]
+      );
+      userId = insertResult.rows[0].id;
+    } else {
+      userId = dbResult.rows[0].id;
+    }
+
+    // case where no user exists!
+
     const newlisting = await pool.query(
       `INSERT INTO list (donor_id, title, description) values ( $1, $2, $3 )`,
-      [1, title, description]
+      [userId, title, description]
     );
     res.status(201).json("OK - list was updated");
   } catch (error) {
