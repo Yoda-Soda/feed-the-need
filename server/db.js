7e3cf1d1c839dd870b8b97daf3b498e176ef5424
castleArg <markchurcher@gmail.com>
2020-10-30 22:16:55 +1300

    switch to conn string in production

--- server/db.js
+++ server/db.js
@@ -4,7 +4,8 @@ var types = require('pg').types
 types.setTypeParser(1700, function(val) {
   return parseFloat(val)
 });
-const pool = new Pool();
+
+const pool = process.env.NODE_ENV === 'production' ? new Pool({connectionString: process.env.DATABASE_URL})  :  new Pool();
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
