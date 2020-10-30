99edfee163d9cf44e81ea7e309bfeaaebf13dcf6
castleArg <markchurcher@gmail.com>
2020-10-31 07:59:47 +1300

    wildcard route fix

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", jwtCheck, apiRouter);
 
 app.use(express.static(path.join(__dirname, 'ui')));
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
