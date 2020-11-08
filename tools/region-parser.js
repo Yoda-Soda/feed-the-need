cb4c8d8d8b3efbbe1f1b0296919eb93e96c5dfd7
Yoda-Soda <janclaasen@gmail.com>
2020-11-09 07:49:06 +1300

    using remote CSV file and using papaparse for parsing

--- tools/region-parser.js
+++ tools/region-parser.js
@@ -1,23 +1,33 @@
-const fs = require("fs");
-const path = require("path");
-const csv = require("fast-csv");
 const { createNewRegion } = require("../server/dataAccess/regionRepository");
+const Papa = require("papaparse");
+const fetch = require("node-fetch");
 
 const startParser = async () => {
-  await fs
-    .createReadStream(
-      path.resolve(__dirname, "./fire-and-emergency-nz-localities.csv")
-    )
-    .pipe(csv.parse({ headers: true }))
-    .on("error", (error) => console.error(error))
-    .on("data", (row) => {
-      //console.log(row);
-      if (row.type === "SUBURB") {
-        console.log(row.city_name, "-", row.suburb_4th_order);
-        //createNewRegion(row.city_name, row.suburb_4th_order);
+  const regionResponse = await fetch(
+    "http://feed-the-need-data.s3-website-ap-southeast-2.amazonaws.com/"
+  ); // fetch csv todo: change fetch to a streaming download
+  const regionCSV = await regionResponse.text();
+  let count = 1;
+  // parse into objects
+  await Papa.parse(regionCSV, {
+    header: true,
+    step: function (row) {
+      //filter object for data
+      if (row.data.type === "SUBURB") {
+        console.log(
+          count++,
+          row.data.majorlocality_name,
+          "-",
+          row.data.suburb_4th_order
+        );
+        // insert data into DB
+        createNewRegion(row.data.majorlocality_name, row.data.suburb_4th_order);
       }
-    })
-    .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`));
+    },
+    complete: function () {
+      console.log("Parsing completed and data inserted into DB");
+    },
+  });
 };
 
 startParser();
