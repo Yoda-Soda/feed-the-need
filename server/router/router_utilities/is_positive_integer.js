5b611079b55e7d49abeb797afc39664381a58c24
JohnBlasingame <61126121+JohnBlasingame@users.noreply.github.com>
2020-11-12 11:33:06 +1300

    Corrected testing for decimals that are whole numbers

--- server/router/router_utilities/is_positive_integer.js
+++ server/router/router_utilities/is_positive_integer.js
@@ -1,5 +1,6 @@
 /*  Check if customer id is not a positive integer */
+// FIXME: this looks dirty
 const isPositiveInt = (intToBeTested) =>
-  intToBeTested > 0 && Number.isInteger(Number(intToBeTested));
+  intToBeTested > 0 && Number.isInteger(Number(intToBeTested)) && !String(intToBeTested).includes('.');
 
 exports.isPositiveInt = isPositiveInt;
