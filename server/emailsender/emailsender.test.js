4a11886b967d8606b76ab8a3df95d1e47caf347d
Raf Gemmail <raf@developersinstitute.co.nz>
2020-11-12 14:28:32 +1300

    Rafs worked-example-do-not-merge

---
+++ server/emailsender/emailsender.test.js
@@ -0,0 +1,26 @@
+
+const mockSend = jest.fn(()=>new Promise(()=>{}));
+const mockSetApiKey = jest.fn((apiKey)=>{});
+ 
+jest.mock("@sendgrid/mail", () => {
+	return {
+		send: mockSend,
+		setApiKey: mockSetApiKey
+	}
+});
+
+const emailsender= require('./emailsender');
+
+describe("Given that we have a mail sender", () => {
+	describe("When sending an email", () => {
+		test("Then it will use sendgrid to send a mail", () => {
+			emailsender.notifyListingParticipants('foo@test.example', 'bar@test.example');
+			expect(mockSend).toBeCalled();
+		});
+		test("Then it will set the API Key", () => {
+			expect(mockSetApiKey).toBeCalled();
+		});
+	});
+
+});
+
